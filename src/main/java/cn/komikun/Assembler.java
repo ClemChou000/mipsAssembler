1317afff481c81afb3422b943b70aaff5379f49d
Zhang zelin <1632259373@qq.com>
2020-03-13 15:19:02 +0800

    fix bugs

--- src/main/java/cn/komikun/Assembler.java
+++ src/main/java/cn/komikun/Assembler.java
@@ -3,11 +3,8 @@ package cn.komikun;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -19,6 +16,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author ：komikun
@@ -28,7 +27,7 @@ import java.util.regex.Pattern;
  * @version,1.0
  */
 public class Assembler {
-
+  static final Logger logger = LoggerFactory.getLogger(Assembler.class);
   public static void main(String[] args) throws IOException {
     String filepath = "src/main/resources/data.asm";
     File file = new File(filepath);
@@ -40,7 +39,7 @@ public class Assembler {
 }
 
 class Program {
-
+  static final Logger logger = LoggerFactory.getLogger(Program.class);
   private int PC;
   private Map<String, Integer> labelMap;
 
@@ -52,54 +51,58 @@ class Program {
   public String exeProgram(String str) {
     StringBuilder sb = new StringBuilder();
     String[] strs = str.split("(\r\n|\r|\n)", -1);
-    if (!readLabels(strs)){
-      return "Wrong Label";
+    if (!readLabels(strs)) {
+      return "Wrong Label:label must end with ':'";
     }
     for (String s : strs) {
       this.PC += 4;
-      System.out.println("Start parsing :" + s);
+      logger.info("Start parsing :" + s);
       String[] list = s.split(" ");
       // 0 -> label 1 -> orderName 2 -> para 3 -> comment
-      Integer result = 0 ;
-      if (list[1] == null){
+      Integer result = 0;
+      if (list.length == 1) {
         return "Empty Order Name";
       }
-      if (ROrder.isROrder(list[1])) {
+      String orderName = list[1].toLowerCase();
+      if (ROrder.isROrder(orderName)) {
         if (list.length < 3) {
-          result = ROrder.transformOrder2Binary(list[1], new ArrayList<String>());
+          result = ROrder.transformOrder2Binary(orderName, new ArrayList<String>());
         } else {
-          result = ROrder.transformOrder2Binary(list[1], Util.preprocess(list[2]));
+          result = ROrder.transformOrder2Binary(orderName, Util.preprocess(list[2].toLowerCase()));
         }
-      } else if (IOrder.isIOrder(list[1])) {
-        System.out.println(list[2]);
-        result = IOrder.transformOrder2Binary(list[1], Util.preprocess(list[2]), this.PC, this.labelMap);
-      } else if (JOrder.isJOrder(list[1])) {
-        result = JOrder.transformOrder2Binary(list[1],Util.preprocess(list[2]),this.labelMap);
+      } else if (list.length < 3) {
+        result = null;
+      } else if (IOrder.isIOrder(orderName)) {
+        result = IOrder
+            .transformOrder2Binary(orderName, Util.preprocess(list[2].toLowerCase()), this.PC,
+                this.labelMap);
+      } else if (JOrder.isJOrder(orderName)) {
+        result = JOrder.transformOrder2Binary(orderName, Util.preprocess(list[2]), this.labelMap);
       } else {
         result = null;
       }
-      if (result != null){
-        sb.append(String.format("%08x",result));
+      if (result != null) {
+        sb.append("[0x" + String.format("%08x", result) + "] ");
+        sb.append(list[1] + " " + list[2]);
         sb.append("\n");
-      }else{
-        sb.append("No Such Order");
+      } else {
+        sb.append("illegal farmat:" + s);
         sb.append("\n");
       }
 
     }
-    System.out.println(sb.toString());
+    logger.info(sb.toString());
     return sb.toString();
   }
 
-  private Boolean readLabels(String[] str){
+  private Boolean readLabels(String[] str) {
     int lineNum = 0;
     for (String s : str) {
-      String label = s.split(" ")[0];
+      String label = s.split(" ")[0].toLowerCase();
       if (!label.equals("")) {
-        if (label.endsWith(":")){
-          this.labelMap.put(label.substring(0,label.length()-1), lineNum);
-        }
-        else{
+        if (label.endsWith(":")) {
+          this.labelMap.put(label.substring(0, label.length() - 1), lineNum);
+        } else {
           return false;
         }
       }
@@ -108,6 +111,7 @@ class Program {
     return true;
   }
 }
+
 class Order {
 
   static Map<String, Integer> registersMap = ImmutableMap.<String, Integer>builder().put("$zero", 0)
@@ -116,15 +120,21 @@ class Order {
       .put("$t5", 13).put("$t6", 14).put("$t7", 15).put("$s0", 16).put("$s1", 17).put("$s2", 18)
       .put("$s3", 19).put("$s4", 20).put("$s5", 21).put("$s6", 22).put("$s7", 23).put("$t8", 24)
       .put("$t9", 25).put("$k0", 26).put("$k1", 27).put("$gp", 28).put("$sp", 29).put("$fp", 30)
-      .put("$ra", 31).build();
-
+      .put("$ra", 31)
+      .put("$0", 0).put("$1", 1).put("$2", 2).put("$3", 3).put("$4", 4).put("$5", 5).put("$6", 6)
+      .put("$7", 7).put("$8", 8).put("$9", 9).put("$10", 10).put("$11", 11).put("$12", 12)
+      .put("$13", 13).put("$14", 14).put("$15", 15).put("$16", 16).put("$17", 17).put("$18", 18)
+      .put("$19", 19).put("$20", 20).put("$21", 21).put("$22", 22).put("$23", 23).put("$24", 24)
+      .put("$25", 25).put("$26", 26).put("$27", 27).put("$28", 28).put("$29", 29).put("$30", 30)
+      .put("$31", 31).build();
 }
 
 class ROrder extends Order {
+  static final Logger logger = LoggerFactory.getLogger(ROrder.class);
 
   private static Set<String> orders = ImmutableSet.<String>builder()
       .add("add", "addu", "sub", "subu", "slt", "sltu", "and", "or", "xor", "nor", "sll", "srl",
-          "sllv", "srlv", "srav", "mult", "multu", "div", "divu", "jalr", "eret", "syscall","jr")
+          "sllv", "srlv", "srav", "mult", "multu", "div", "divu", "jalr", "eret", "syscall", "jr")
       .build();
 
   public static Boolean isROrder(String orderName) {
@@ -143,10 +153,10 @@ class ROrder extends Order {
         return null;
       case 1:
         rs = paraList.get(0);
-        if (orderName.equals("jr")){
-          if (registersMap.containsKey(rs)){
-            return (0<<26)|(8&63) |(registersMap.get(rs)<<21);
-          }else{
+        if (orderName.equals("jr")) {
+          if (registersMap.containsKey(rs)) {
+            return (0 << 26) | (8 & 63) | (registersMap.get(rs) << 21);
+          } else {
             return null;
           }
         }
@@ -154,7 +164,7 @@ class ROrder extends Order {
       case 2:
         rs = paraList.get(0);
         rt = paraList.get(1);
-        if (!registersMap.containsKey(rs) || !registersMap.containsKey(rt)){
+        if (!registersMap.containsKey(rs) || !registersMap.containsKey(rt)) {
           return null;
         }
         switch (orderName) {
@@ -222,7 +232,7 @@ class ROrder extends Order {
                 return (0 << 26) | (7 & 63) | (registersMap.get(rs) << 11) | (registersMap.get(rt)
                     << 21) | (registersMap.get(rd) << 16);
             }
-          }else if (Util.isNumber(rt)){
+          } else if (Util.isNumber(rt)) {
             switch (orderName) {
               case "sll":
                 return (0 << 26) | (registersMap.get(rd) << 11) | (registersMap.get(rs) << 21) | (
@@ -234,7 +244,7 @@ class ROrder extends Order {
                 return (0 << 26) | (3 & 63) | (registersMap.get(rd) << 11) | (registersMap.get(rs)
                     << 21) | ((Util.str2Num(rt) & 31) << 6);
             }
-          }else{
+          } else {
             return null;
           }
 
@@ -245,6 +255,7 @@ class ROrder extends Order {
 }
 
 class IOrder extends Order {
+  static final Logger logger = LoggerFactory.getLogger(IOrder.class);
 
   private static Set<String> twoArgWithBrackets = ImmutableSet.<String>builder()
       .add("lw", "sw", "lh", "lhu", "sh").build();
@@ -268,20 +279,20 @@ class IOrder extends Order {
 
   public static Integer transformOrder2Binary(String orderName, List<String> paraList, int PC,
       Map<String, Integer> map) {
-    if (twoArgWithBrackets.contains(orderName)) {
+    if (twoArgWithBrackets.contains(orderName) && paraList.size() == 2) {
       String rt = paraList.get(0);
       String tmp = paraList.get(1);
       int start = tmp.indexOf('(');
       int end = tmp.indexOf(')');
-      if(start == -1 || end == -1 || end <= start){
+      if (start == -1 || end == -1 || end <= start) {
         return null;
       }
       String rs = tmp.substring(tmp.indexOf('(') + 1, tmp.indexOf(')'));
       String imm = tmp.substring(0, tmp.indexOf('('));
-      if (imm.equals("")){
+      if (imm.equals("")) {
         imm = "0";
       }
-      if (Util.isNumber(imm) && registersMap.containsKey(rt) && registersMap.containsKey(rs)){
+      if (Util.isNumber(imm) && registersMap.containsKey(rt) && registersMap.containsKey(rs)) {
         switch (orderName) {
           case "lw":
             return (35 << 26) | (registersMap.get(rt) << 16) | (Util.str2Num(imm) & 0xFFFF) | (
@@ -299,55 +310,57 @@ class IOrder extends Order {
             return (41 << 26) | (registersMap.get(rt) << 16) | (Util.str2Num(imm) & 0xFFFF) | (
                 registersMap.get(rs) << 21);
         }
-      }else{
+      } else {
         return null;
       }
     }
-    if (twoArg.contains(orderName)) {
+    if (twoArg.contains(orderName) && paraList.size() == 2) {
       String rt = paraList.get(0);
       String imm = paraList.get(1);
-      if (registersMap.containsKey(rt) && Util.isNumber(imm)){
+      if (registersMap.containsKey(rt) && Util.isNumber(imm)) {
         switch (orderName) {
           case "lui":
             return (15 << 26) | (registersMap.get(rt) << 16) | (Util.str2Num(imm) & 0xFFFF);
           case "bgezal":
             return (1 << 26) | ((17 & 31) << 16) | (registersMap.get(rt) << 21) | (
-                ((Util.str2Num(imm) - PC - 2) >> 1) & 0xFFFF);
+                ((Util.str2Num(imm) - PC) >> 2) & 0xFFFF);
         }
-      }else{
+      } else {
         return null;
       }
 
     }
-    if (threeArgWithLabel.contains(orderName)) {
+    if (threeArgWithLabel.contains(orderName) && paraList.size() == 3) {
       String rs = paraList.get(0);
       String rt = paraList.get(1);
       String label = paraList.get(2);
       Integer address;
       if (map.containsKey(label)) {
         address = map.get(label);
+      }else if (Util.isNumber(label)){
+        address = Util.str2Num(label);
       } else {
         return null;
       }
-      if(registersMap.containsKey(rs) && registersMap.containsKey(rt)){
+      if (registersMap.containsKey(rs) && registersMap.containsKey(rt)) {
         switch (orderName) {
           case "beq":
             return (4 << 26) | (registersMap.get(rs) << 21) | (registersMap.get(rt) << 16) | (
-                ((address - PC - 2) >> 1) & 0xFFFF);
+                ((address - PC) >> 2) & 0xFFFF);
           case "bne":
             return (5 << 26) | (registersMap.get(rs) << 21) | (registersMap.get(rt) << 16) | (
-                ((address - PC - 2) >> 1) & 0xFFFF);
+                ((address - PC) >> 2) & 0xFFFF);
         }
-      }else{
+      } else {
         return null;
       }
 
     }
-    if (threeArg.contains(orderName)) {
+    if (threeArg.contains(orderName) && paraList.size() == 3) {
       String rt = paraList.get(0);
       String rs = paraList.get(1);
       String imm = paraList.get(2);
-      if (registersMap.containsKey(rt) && registersMap.containsKey(rs) && Util.isNumber(imm)){
+      if (registersMap.containsKey(rt) && registersMap.containsKey(rs) && Util.isNumber(imm)) {
         switch (orderName) {
           case "addi":
             return (8 << 26) | (registersMap.get(rt) << 16) | (registersMap.get(rs) << 21) | (
@@ -365,7 +378,7 @@ class IOrder extends Order {
             return (14 << 26) | (registersMap.get(rt) << 16) | (registersMap.get(rs) << 21) | (
                 Util.str2Num(imm) & 0xFFFF);
         }
-      }else{
+      } else {
         return null;
       }
 
@@ -374,38 +387,44 @@ class IOrder extends Order {
   }
 
 }
-class JOrder extends Order{
-  static Set<String> orders = ImmutableSet.<String>builder().add("j","jal").build();
-  static Boolean isJOrder(String orderName){
+
+class JOrder extends Order {
+  static final Logger logger = LoggerFactory.getLogger(JOrder.class);
+
+  static Set<String> orders = ImmutableSet.<String>builder().add("j", "jal").build();
+
+  static Boolean isJOrder(String orderName) {
     return orders.contains(orderName);
   }
-  static Integer transformOrder2Binary(String orderName , List<String> paraList,Map<String,Integer> map){
-    if(orderName.equals("j")){
+
+  static Integer transformOrder2Binary(String orderName, List<String> paraList,
+      Map<String, Integer> map) {
+    if (orderName.equals("j")) {
       String label = paraList.get(0);
       int address;
-      if (map.containsKey(label)){
+      if (map.containsKey(label)) {
         address = map.get(label);
-        return (2<<26) |((address)>>1)&0x3FFFFFF;
-      }else if(Util.isNumber(label)){
-        return (2<<26) |((Util.str2Num(label))>>1)&0x3FFFFFF;
-      }else{
+        return (2 << 26) | ((address) >> 1) & 0x3FFFFFF;
+      } else if (Util.isNumber(label)) {
+        return (2 << 26) | ((Util.str2Num(label)) >> 1) & 0x3FFFFFF;
+      } else {
         return null;
       }
-    }else if (orderName.equals("jal")){
-      if (registersMap.containsKey(paraList.get(0))){
-        return (3<<26) |((registersMap.get(paraList.get(0))>>1)&0x3FFFFFF);
-      }else{
+    } else if (orderName.equals("jal")) {
+      if (registersMap.containsKey(paraList.get(0))) {
+        return (3 << 26) | ((registersMap.get(paraList.get(0)) >> 1) & 0x3FFFFFF);
+      } else {
         return null;
       }
     }
     return null;
   }
 }
-
 
 
 class Util {
-  static Boolean isNumber(String str){
+  static final Logger logger = LoggerFactory.getLogger(Util.class);
+  static Boolean isNumber(String str) {
     Pattern pattern = Pattern.compile("-?[0-9]+(\\.[0-9]+)?");
     String bigStr;
     try {
@@ -422,7 +441,7 @@ class Util {
 
   static void printArray(String[] arr) {
     for (String s : arr) {
-      System.out.println(s + ",");
+      logger.info(s+",");
     }
   }
 
